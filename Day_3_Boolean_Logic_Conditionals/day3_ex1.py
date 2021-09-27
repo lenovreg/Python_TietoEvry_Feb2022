663b25e3a80246b80aeacc2b4797cede03960305
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-09-27 17:40:51 +0300

    First Exercise optimized

--- Day_3_Boolean_Logic_Conditionals/day3_ex1.py
+++ Day_3_Boolean_Logic_Conditionals/day3_ex1.py
@@ -3,7 +3,7 @@
 temperature = float(input("What's your temperature?"))
 if temperature < 35:
     print("not too cold")
-elif 35 <= temperature <= 37: # so  inclusive same as 35 <= temperature and temperature <= 37:
+elif temperature <= 37:  # so  inclusive same
     print("all right")
 else:
     print("possible fever")
