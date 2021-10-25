d0d7a43120254db697431e20be68e4fffd3b3d0d
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-10-25 15:12:45 +0300

    Add Day 10 Rap inheritance from Song

--- Day_10_classes_and_objects/d10_u1.py
+++ Day_10_classes_and_objects/d10_u1.py
@@ -6,7 +6,7 @@ class Song:
         print(f"{title} by {author} - created {len(self.lyrics)} lines")
 
     # _ significies that this method is for internal use by the other methods in the object
-    def _print_lyrics(self, lyrics, max_lines=-1): # these lyrics will be processed already
+    def _print_lyrics(self, lyrics, max_lines=-1):  # these lyrics will be processed already
         if max_lines == -1:
             max_lines = len(lyrics)
         for i in lyrics[:max_lines]:
@@ -34,3 +34,28 @@ vairogi = Song("Vairogi", "Līvi", ["Mūsu dziesmas ir vairogi seni", "Mēs tās
 vairogi.sing(2).yell(1)
 
 vairogi.sing().sing().sing()
+
+
+class Rap(Song):
+
+    # no need for custom __init__ in this case
+    def break_it(self, max_lines=-1, drop="yeah"):
+        print("Singer:", self.author, "-", self.title)
+        for line in self.lyrics:
+            print(line.replace(' ', f' {drop} '), sep="\n")
+            max_lines -= 1
+            if max_lines == 0:
+                break
+        return self
+
+
+ziemelmeita = Rap("Ziemeļmeita", "Jumprava", ["Gāju meklēt ziemeļmeitu", "Garu, tālu ceļu veicu"])
+ziemelmeita.break_it(1).break_it()
+
+ziemelmeita.sing().yell().break_it()
+
+livi = Rap("Vairogi", "Līvi", ["Mūsu dziesmas ir vairogi seni", "Mēs tās par pagalvjiem liksim",
+                                   "Daudzu likteņu pilni mēs elposim", "Kā pakalni Daugavas krastos"])
+
+livi.sing().yell(2).break_it(drop="Jā!").break_it(2, "ahh")
+
