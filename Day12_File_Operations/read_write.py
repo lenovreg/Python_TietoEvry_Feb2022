d134ef56f376e5e81d4eacd0e33d0aa786172012
Valdis Saulespurens <valdis.s.coding@gmail.com>
2021-10-29 16:20:10 +0300

    Day 12 File Operations

--- Day12_File_Operations/read_write.py
+++ Day12_File_Operations/read_write.py
@@ -1,33 +1,57 @@
 # # # # # # # # # Reading and writing files
 # # # # # # # # # https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # # # # # # # # # https://realpython.com/read-write-files-python/
-# # # # # # # # # file - set of continuos bytes for storing data - various formats
+# # # # # # # # # file - set of continuous bytes for storing data - various formats
 # # # # # # # # # libraries for many common formats (CSV, JSON, etc)
 # # # # # # # # # plain text files - encodings and line endings could be different
 # # # # # # # # # usually we have Unicode today (encoding using UTF-8)
 # # # # # # # # # creating file object (file stream)
 # # # # # # # # # closing file automatically with with contextcd
-import os
-# import string
-# from datetime import datetime as dt  #datetime has datetime submodule(klase)
-# from pathlib import Path # this is newer for path manipulation
+import os  #system library
+import string
+from datetime import datetime as dt  #datetime has datetime submodule(klase)
+from pathlib import Path # this is newer for path manipulation
 #
 print(os.getcwd())
+print(os.listdir())  # an older way of getting file directory
+print(os.listdir(os.getcwd()))  # exactly the same as above
+files = Path(".").glob("*")  # this is newer way of getting file directory
+# above is a generator object so it needs to be converted to list
+files = list(files)
+print(files)
+# difference is that Path will handle OS specific path separators
 
 #
 # # # # # # # # # # # with - context manager
 #
-# # # # # # using absolute path and manual file closing.. two things we usually want to avoid
-# fstream = open('C:/Users/val-wd/Github/Python_TietoEvry_Sep2021/Day12_File_Operations/frost.txt')
+# abs_path = os.path.abspath("frost.txt")
+# print(abs_path)  # everybody will have their own unique absolute path
+# # # # # # # using absolute path and manual file closing.. two things we usually want to avoid
+# fstream = open(abs_path)  # r is not required it is the default
+# # fstream is a file stream object, it is not the content of the file
+# # i have opened a file and i want to read it
 # text = fstream.read() # read into string
 # fstream.close() # very important to close the file
-# print(text[:100])
+# print(text[:100])  # printing first 100 characters
 #
 #
 # # # # # # # # # we use with to guarantee closing of files
 # # # # # ## Absolute path will be different for pretty much everyone (different user name for one)
 
-#
+# Relative path example
+# fstream = open("frost.txt")  # so this is relative to current working directory
+# text = fstream.read()
+# fstream.close()
+# print(text[:100])
+
+# a recommended approach is to use a context manager
+# with open("frost.txt") as fstream:  # reading mode is implied
+#     text = fstream.read()
+    # fstream is still open
+# fstream is already closed
+# print(text)
+
+
 # # # # # # # Relative Paths
 # # # # # # # # if current directory is one level above our desired
 # # # with open('Diena_12_Faili/frost.txt') as f:  # create a file object, default is read mode
@@ -35,16 +59,17 @@ print(os.getcwd())
 # # # # # # # # important! f is closed here already!!! it's a good thing
 # # # os.chdir("Diena_12_Faili") # so we change our working directory to one level deeper
 # # # # # # # one way of getting to current path is simply change in terminal where we start
-# # with open('frost.txt') as f:  # create a file object, default is read mode
-# #     print(f.read())  # so keep in mind this might not be best for HUUGe files
-# #     print("File is still open here")
-# #     print("Trying again")
-# #     print("Should have ----- new poem below -----")
-# #     print(f.read())  # no error but where is the content ?
-# #     print("Lets try again")
-# #     f.seek(0)  # put the needle of the record to beginning
-# #     print(f.read(20)) # so read 20 symbols from current needle position
-# #     print(f.read(10))  # so this is the method you can use for fine reading
+# with open('frost.txt') as f:  # create a file object, default is read mode
+#     print(f.read())  # so keep in mind this might not be best for HUUGe files
+#     print("File is still open here")
+#     print("Trying again")
+#     print("Should have ----- new poem below -----")
+#     print(f.read())  # no error but where is the content ?
+#     print("Lets try again")
+#     f.seek(0)  # put the needle of the record to beginning
+#     print(f.read(20)) # so read 20 symbols from current needle position
+#     print(f.seek(15))  # return the needle position
+#     print(f.read(5))  # so this is the method you can use for fine reading
 # # # # # # #     print(f.seek(50)) # so jumpe the needle to 50 from 0
 # # # # # # #     print(f.read(15)) # so read from 51 to 65
 # # # # # # #     # f is still open here
@@ -56,8 +81,8 @@ print(os.getcwd())
 # # # # # # # print(res)
 #
 # # # # one level up just add more ../ for more levels
-# # with open("../LICENSE") as f:
-# #     print(f.read())
+# with open("../LICENSE") as f:  # .. means one level up relative to our current working directory
+#     print(f.read())
 #
 # # # # my_files = os.listdir("./") # current dir
 # # # # print(my_files)
@@ -68,19 +93,23 @@ print(os.getcwd())
 #
 # # # # more modern is Path - better compatibility across OSes Windows/Mac/Linux
 # # # # so going through current directory recursively getting all objects ending with .txt and making sure they are files
-# # text_files = [f for f in Path("./").rglob("*.txt") if f.is_file()]  #rglob is recursive goes through subfolders
-# # print(text_files)
+text_files = [f for f in Path("./").rglob("*.txt") if f.is_file()]  #rglob is recursive goes through subfolders
+print(text_files)
 # # # # # # from one level up recursively all text files in my project (one level up)
-# # text_files = [f for f in Path("../.").rglob("*.txt") if f.is_file()]
-# # print(text_files)
+text_files = [f for f in Path("../.").rglob("*.txt") if f.is_file()]
+print(text_files)
+python_files = [f for f in Path("../.").rglob("*.py") if f.is_file()]
+# print(python_files)  # a list of all python files in one level up
+
+
 # # # # # from one level up just all text files in that level just glob
 # # # text_files = [f for f in Path("../.").glob("*.txt") if f.is_file()]
 # # # print(text_files)
 # # current_text_files  = [f for f in Path("./").glob("*.txt") if f.is_file()]
 # # # print(current_text_files)
-# my_path = Path("frost.txt")  # so this will create correct name on all 3 OSes
-# print(my_path)
-#
+my_path = Path("frost.txt")  # so this will create correct name on all 3 OSes
+print(my_path)
+
 # with open(my_path) as f:
 #     txt = f.read()
 # # file is closed here
@@ -103,12 +132,12 @@ print(os.getcwd())
 # # # # # # # # # # # so with open('file.txt') as f: # we are opening file in read mode
 #
 # # # # # # # # with open('Diena_12_Faili/frost.txt', encoding="utf-8") as f:  # create a file object
-# # with open('frost.txt', encoding="utf-8") as f:  # create a file object
-# #     for line in f:  # so this will work even on huge files as long as each line ends with \n
-# #         # print(line)  # of course for big files we will not want to print
-# #         print(line, end="")  # of course for big files we will not want to print
+# with open('frost.txt', encoding="utf-8") as f:  # create a file object
+#     for line in f:  # so this will work even on huge files as long as each line ends with \n
+        # print(line)  # of course for big files we will not want to print
+        # print(line, end="")  # of course for big files we will not want to print
 # # #         print(line.rstrip())  # get rid of all whitespace on right side
-# # #         print(line.rstrip("\n"))  # most precise
+#         print(line.rstrip("\n"))  # most precise strips newline on right side
 # # # # # # # #         # quick and dirty and generally okay possible last line has no \n
 # # #         print(line[:-1]) # possibly cut last char from last line
 #
@@ -140,10 +169,18 @@ print(os.getcwd())
 #
 # # # # # # print(mylines[:5])
 #
-# needle = "roads" # what i want to find
+needle = "roads" # what i want to find
 # # # # # # # with open('Diena_12_Faili/frost.txt', encoding="utf-8") as f:  # create a file object
-# with open('frost.txt', encoding="utf-8") as f:  # create a file object
-#     filtered_lines = [line for line in f if needle in line]
+with open('frost.txt', encoding="utf-8") as f:  # create a file object
+    # filtered_lines = [line for line in f if needle in line]
+    # i cant do second loop without seek
+    # same as above list comprehension
+    filtered_lines = []
+    for line in f:
+        if needle in line:
+            filtered_lines.append(line)
+
+print(filtered_lines)
 # # # # # # # # # # #         # could also read a few lines with f.readline()
 # # # # # # # #     # remember that going through file again would require f.seek(0)
 # #     # filtered_lines_num = [(i, line.rstrip('\n'))
@@ -152,14 +189,14 @@ print(os.getcwd())
 # print(filtered_lines)
 # # # # i want to save my filtered lines
 # # # # mode = w will overwrite old fiile, no error
-# now = dt.now()  #timestampe fixed here
-# print(now)
+now = dt.now()  #timestampe fixed here
+print(now)
 # # # # # timestamp in file name
-# file_path = Path("frost_cleaned_o27.txt")
-# # file_path = Path(f"frost_{needle}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.txt")
-# print(file_path)
-# print(file_path.stem) #without extension this is offer by Path
-# print(file_path.suffix)
+file_path = Path("frost_cleaned_o29.txt")
+# file_path = Path(f"frost_{needle}_{now.month}_{now.day}_{now.hour}_{now.minute}_{now.second}.txt")
+print(file_path)
+print(file_path.stem) #without extension this is offer by Path
+print(file_path.suffix)
 #
 # if file_path.is_file(): # this is offered by Path from standard library module pathlib
 #     print(f"Sorry {file_path} exists, so not going to overwrite") # should be very rare once a year ...
@@ -188,32 +225,32 @@ print(os.getcwd())
 #
 # # # # # # # # # # # how to write a file?
 # # # # # # # # # # # with mode = "w" file will be created or overwritten
-# # # with open('frost-filtered.txt', mode="w", encoding="utf-8") as f:
-# # #     f.write("My filtered file\n")
-# # #     f.write("\n"*2)
-# # #     f.writelines(filtered_lines)  # remember to check if you need \n
-# # #     f.writelines([line+"\n" for line in filtered_lines]) # including last one
-# # #     f.write("*"*40+"\n")
-# # #     f.write("\n".join(filtered_lines)) # without newline in last line
-# # #     f.write("*"*40+"\n")
-# # #     f.write("".join(filtered_lines)) # without newline in last line
+# with open('frost-filtered.txt', mode="w", encoding="utf-8") as f:
+#     f.write("My filtered file\n")
+#     f.write("\n"*2)
+#     f.writelines(filtered_lines)  # remember to check if you need \n
+#     f.writelines([line+"\n" for line in filtered_lines]) # including last one
+#     f.write("*"*40+"\n")
+#     f.write("\n".join(filtered_lines)) # without newline in last line
+#     f.write("*"*40+"\n")
+#     f.write("".join(filtered_lines)) # without newline in last line
 # # # # # # # # # # # # # file should be closed here, crucial for writing
 # # # # # # import datetime
 # # # # # # # mode="a" is for append to the file
 # # now = dt.now()
-# print("Now will append to ", file_path)
-# with open(file_path, mode="a", encoding="utf-8") as f:
-#     f.write("\n"+"*"*40+"\n")
+print("Now will append to ", file_path)
+with open(file_path, mode="a", encoding="utf-8") as f:
+    f.write("\n"+"*"*40+"\n")
 #
-#     f.write(str(now)+"\n")  #wrote a timestamp into a file
-#     for n in range(5):
-#         f.write(f"{n}\n")
-#     my_list = list("abba")
-#     f.write("\n".join(my_list))  #another way how to add list elements in new lines
-#     f.write("\n")
-#     for line in filtered_lines:
-#         f.write(line+"\n")
-#     print("Printing into faile", file=f)  # this requires that f is open of course
+    f.write(str(now)+"\n")  #wrote a timestamp into a file
+    for n in range(5):
+        f.write(f"{n}\n")
+    my_list = list("abba")
+    f.write("\n".join(my_list))  #another way how to add list elements in new lines
+    f.write("\n")
+    for line in filtered_lines:
+        f.write(line+"\n")
+    print("Printing into faile", file=f)  # this requires that f is open of course
 #
 # # # # print(now.day, now.year, now.minute, now.second)
 # # # now = dt.now()
